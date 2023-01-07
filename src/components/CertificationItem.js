3be244b387509efe13348422abbacfc9be3c94fb
Kathy Negrete A <kathynegretea@Kathys-MacBook-Pro.local>
2023-01-07 15:43:18 -0600

    content(code):added a hover funtion

--- src/components/CertificationItem.js
+++ src/components/CertificationItem.js
@@ -9,18 +9,25 @@ const CertificationItem = () => {
       <Certifications
         certLink="https://www.coursera.org/account/accomplishments/verify/NP4AMAFV3LXE?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course"
         certImage={certificationImage(`./cert-html.png`)}
+        name="HTML5"
+
       />
       <Certifications
         certLink="https://www.coursera.org/account/accomplishments/verify/LAJKVHPMLVFY?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course"
         certImage={certificationImage(`./cert-css.png`)}
+        name="CSS3"
+
       />
       <Certifications
         certLink="https://www.coursera.org/account/accomplishments/verify/M5VPPRTG3SRK?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course"
         certImage={certificationImage(`./cert-js.png`)}
+        name="JavaScript"
+
       />
       <Certifications
         certLink="https://www.coursera.org/account/accomplishments/verify/HQEN6SLXFFWE?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course"
         certImage={certificationImage(`./cert-rd.png`)}
+        name="Responsive Design"
       />
     </div>
   );
